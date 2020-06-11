030fac669ae2bec5e9da4ceb2d0d48d0c91ba640
smokejohn <johannesrauch91@gmail.com>
2020-06-11 03:10:36 +0200

    adds qtwiki custom signal slot example

--- window.cpp
+++ window.cpp
@@ -1,18 +1,36 @@
 #include "window.h"
-#include <QApplication>
 #include <QPushButton>
-
+#include <QApplication>
 
 Window::Window(QWidget *parent) :
     QWidget(parent)
 {
     // Set size of the window
-    setFixedSize(200, 100);
+    setFixedSize(100, 50);
 
-    // Create and position the button
+    // Create and position button
     m_button = new QPushButton("Hello World", this);
     m_button->setGeometry(10, 10, 80, 30);
+    m_button->setCheckable(true);
 
-    // NEW: Do Signal Slot Connection to QApplication Instance
-    connect(m_button, SIGNAL(clicked()), QApplication::instance(), SLOT(quit()));
+    m_counter = 0;
+
+    QObject::connect(m_button, SIGNAL(clicked(bool)), this, SLOT(slotButtonClicked(bool)));
+    QObject::connect(this, SIGNAL(counterReached()), QApplication::instance(), SLOT(quit()));
+}
+
+void Window::slotButtonClicked(bool checked)
+{
+    if (checked) {
+        m_button->setText("Checked");
+    }
+    else {
+        m_button->setText("Hello World");
+    }
+
+    m_counter++;
+
+    if (m_counter == 10) {
+        emit counterReached();
+    }
 }
--- window.h
+++ window.h
@@ -2,18 +2,21 @@
 #define WINDOW_H
 
 #include <QWidget>
-#include <QPushButton>
 
+class QPushButton;
 class Window : public QWidget
 {
     Q_OBJECT
 
     public:
         explicit Window(QWidget *parent = 0);
-
+    signals:
+        void counterReached();
+    private slots:
+        void slotButtonClicked(bool checked);
     private:
+        int m_counter;
         QPushButton *m_button;
-
 };
 
 #endif // WINDOW_H
