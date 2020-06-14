9d9770a3488b2f2496ee7be2ba9b4266669059b7
smokejohn <johannesrauch91@gmail.com>
2020-06-15 01:31:43 +0200

    adds all exercises for chapter 5 - Files and Directories | zetcode.com

---
+++ zet_5/file_size.cpp
@@ -0,0 +1,31 @@
+#include <QTextStream>
+#include <QFileInfo>
+
+int main(int argc, char *argv[]) {
+
+    QTextStream out(stdout);
+
+    if (argc != 2) {
+        
+        qWarning("Usage: file_size file");
+        return 1;
+    }
+
+    QString filename = argv[1];
+
+    if (!QFile(filename).exists()) {
+
+        qWarning("The file does not exist");
+        return 1;
+    }
+
+    QFileInfo fileinfo(filename);
+
+    qint64 size = fileinfo.size();
+
+    QString str = "The size is: %1 bytes";
+
+    out << str.arg(size) << endl;
+
+    return 0;
+}
---
+++ zet_5/special_paths.cpp
@@ -0,0 +1,15 @@
+#include <QTextStream>
+#include <QDir>
+
+int main(void) {
+
+    QTextStream out(stdout);
+
+    out << "Current path:" << QDir::currentPath() << endl;
+    out << "Home path:" << QDir::homePath() << endl;
+    out << "Temporary path:" << QDir::tempPath() << endl;
+    out << "Root path:" << QDir::rootPath() << endl;
+
+    return 0;
+
+}
