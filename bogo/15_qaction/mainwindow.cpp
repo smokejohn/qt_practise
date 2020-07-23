ba9f410328ef84b8b46b6563ca31e45ef9910cb1
smokejohn <johannesrauch91@gmail.com>
2020-07-23 02:15:22 +0200

    adds all files for chapter - QAction | bogotobogo.com

---
+++ bogo/15_qaction/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+    QMessageBox msgBox;
+    msgBox.setText("save action");
+    msgBox.exec();
+}
